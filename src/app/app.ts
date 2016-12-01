88cf0aa61516b6e94b2866750551598936558deb
Stefani Nikolova <Stefani Nikolova>
2016-12-01 19:50:24 +0200

    Project skeleton Angular2

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app',
+    template: `
+            <div>
+            <h1>Causify DEMO</h1>
+            </div> 
+          `
+})
+export class App{
+
+}
